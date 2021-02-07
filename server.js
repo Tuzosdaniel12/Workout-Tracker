05272e884701b5613e15de2397c2e005ff678d50
daniel <danielsoledad@gmail.com>
2021-02-07 15:26:51 -0800

    adding comments to server.js

--- server.js
+++ server.js
@@ -2,19 +2,25 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 
+//heroku app
 const PORT = process.env.PORT || 3000;
 
+//set app to use express
 const app = express();
 
 app.use(logger("dev"));
 
+//middleware
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+//static public folder
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethods", { useNewUrlParser: true });
+//mongoose database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_tracker_db", { useNewUrlParser: true });
 
+//require routes
 app.use(require("./routes"));
 
 app.listen(PORT, () => {
