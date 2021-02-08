c9a8f291052a250dbb002f273df641daeeb11518
daniel <danielsoledad@gmail.com>
2021-02-08 14:56:33 -0800

    Adding dotenv

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,8 @@
 let mongoose = require("mongoose");
 let db = require("../models");
+require("dotenv").config();
 
-mongoose.connect("mongodb://localhost/workout_tracker_db", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_tracker_db", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
