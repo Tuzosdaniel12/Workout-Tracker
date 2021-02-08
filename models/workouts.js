e18188db34f2d3830b86018b184f448d0e1efe80
daniel <danielsoledad@gmail.com>
2021-02-07 23:10:23 -0800

    changed my schema to match seeder, added gets routes, still going have to figuere out hoe to range data ,added post and put routes as well

--- models/workouts.js
+++ models/workouts.js
@@ -3,35 +3,40 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutsSchema = new Schema({
-    type: {
-      type: String,
-      trim: true,
-      required: "Type is Required"
+    day:{
+        type: Date,
+        default: Date.now
     },
-  
-    name: {
-      type: String,
-      trim: true,
-      required: "Name is Required"
-    },
-  
-    weight: {
-      type: Number,
-    },
-  
-    sets: {
-      type: Number
-    },
-    reps: {
-        type: Number
-    },
-    duration: {
-        type: Number
-    },
-    distance: {
-        type: Number
-    }
-
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Type is Required"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Name is Required"
+            },
+            weight: {
+                type: Number,
+            },
+        
+            sets: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            duration: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            } 
+        }
+    ]
   });
   
   const Workouts = mongoose.model("Workouts", WorkoutsSchema);
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,50 @@
 const router = require("express").Router();
 const { Workouts } = require("../models");
 
-router.get("/api/workouts", (req, res) =>{
+router.get("/api/workouts", async (req, res) =>{
+    try {
+        const workout = await Workouts.find({});
 
+        res.json(workout)
+
+    } catch (error) {
+        res.json(error);
+    }
 });
+
+router.get("/api/workouts/range", async (req, res)=>{
+    try {
+        
+        const rangeWorkouts = await Workouts.find({});
+
+        res.json(rangeWorkouts)
+
+    } catch (error) {
+
+        res.json(error);
+    }
+})
+
+router.put("/api/workouts/:id", async ({body, params}, res)=>{
+   
+    try {
+        const updateWorkout = await Workouts.where({ _id: params.id }).update({exercises: body});
+        console.log(updateWorkout)
+
+        res.json(updateWorkout);
+
+    } catch (error) {
+        res.json(error);
+    }
+})
+
+router.post("/api/workouts", async ({body}, res)=>{
+    try {
+        const createWorkout = await Workouts.create(body);
+        res.json(createWorkout);
+    } catch (error) {
+        res.json(err);
+    }
+})
 
 module.exports = router;
